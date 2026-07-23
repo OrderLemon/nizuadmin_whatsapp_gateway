113e531966c16cd912038ec5a22665cbadf87c17
olek1305 <olek1305@gmail.com>
2026-07-23 11:54:43 +0200

    feat: add messages_stats task #8198

---
+++ v2/plugins/Messages_stats/src/Messages_statsPlugin.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Plugins\Messages_stats;
+
+use Plugins\Messages_stats\Controllers\StatsController;
+use Pmsrapi\V2\Core\Container;
+use Pmsrapi\V2\Database\Connection;
+use Pmsrapi\V2\Http\Request;
+use Pmsrapi\V2\Http\Response;
+use Pmsrapi\V2\Plugin\AbstractPlugin;
+use Pmsrapi\V2\Plugin\PluginRegistrar;
+use Pmsrapi\V2\Plugin\PluginRouter;
+
+final class Messages_statsPlugin extends AbstractPlugin
+{
+    public function register(PluginRegistrar $registrar): void
+    {
+        $registrar->singleton(
+            StatsController::class,
+            static fn(Container $c): StatsController => new StatsController(
+                $c->get(Connection::class),
+            ),
+        );
+    }
+
+    public function routes(PluginRouter $router, Container $container): void
+    {
+        $router->get('/', static fn(Request $r, array $p): Response
+            => $container->get(StatsController::class)->stats($r));
+    }
+}
