0d4c60a53e2ad60d79b6c86b0018d9143e634baf
Ruvenss G. Wilches <ruvenss@gmail.com>
2026-09-17 16:15:28 +0200

    Refactor: update configuration file path handling in daily cron job script

--- v1/cron/day.php
+++ v1/cron/day.php
@@ -3,9 +3,11 @@
  * Console command to run daily cron jobs
  */
 $start_time = microtime(true);
-require_once getcwd() . '/../config.php';
-if (file_exists(config_path)) {
-    $configContent = file_get_contents(config_path);
+$v1_dir = dirname(__DIR__);
+require_once $v1_dir . '/config.php';
+$secrets_path = str_starts_with(config_path, '/') ? config_path : $v1_dir . '/' . config_path;
+if (file_exists($secrets_path)) {
+    $configContent = file_get_contents($secrets_path);
     $configData = json_decode($configContent, true);
     if (json_last_error() === JSON_ERROR_NONE) {
         define("ms_secrets", $configData);
@@ -18,17 +20,17 @@ if (file_exists(config_path)) {
     }
 } else {
     define("ms_secrets", []);
-    echo "Configuration file not found at " . getcwd() . '/../config.php';
+    echo "Configuration file not found at " . $secrets_path;
     exit(1);
 }
-if (file_exists(getcwd() . '/../vendor/autoload.php')) {
-    include_once getcwd() . '/../vendor/autoload.php';
+if (file_exists($v1_dir . '/vendor/autoload.php')) {
+    include_once $v1_dir . '/vendor/autoload.php';
 }
-if (isset(ms_secrets['db']) && file_exists(getcwd() . '/../general/db.php')) {
-    include_once getcwd() . '/../general/db.php';
+if (isset(ms_secrets['db']) && file_exists($v1_dir . '/general/db.php')) {
+    include_once $v1_dir . '/general/db.php';
 }
-if (file_exists(getcwd() . '/../general/custom_functions.php')) {
-    include_once getcwd() . '/../general/custom_functions.php';
+if (file_exists($v1_dir . '/general/custom_functions.php')) {
+    include_once $v1_dir . '/general/custom_functions.php';
 }
 function collect_conversations_per_day(){
     /* 
